f374fac96216696f7a70a95fc6ba03cd623d9278
Pavan <your@email.com>
2026-03-27 11:49:32 +0530

    added full project

---
+++ src/main/java/com/klu/model/Book.java
@@ -0,0 +1,52 @@
+package com.klu.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private double price;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+    	
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/klu/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.klu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.klu.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
